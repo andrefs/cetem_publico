86b38241d33496c46995e48b1274ca38ffef6d95
André Santos <andrefs@andrefs.com>
2019-11-15 14:02:26 +0000

    Add repo info to setup

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@
 import setuptools
 
 with open("README.md","r") as fh:
-  longdescription = fh.read() 
+  longdescription = fh.read()
 
 setuptools.setup(name='cetem_publico',
-      version='0.0.6',
-      description='Python WRAPPER For CETEM Publico Corpus',
-      author='André Santos',
-      author_email='afs@inesctec.pt',
-      packages = setuptools.find_packages(),
-      long_description = longdescription
-     )
+    version='0.0.7',
+    description='Python WRAPPER For CETEM Publico Corpus',
+    author='André Santos',
+    author_email='afs@inesctec.pt',
+    packages = setuptools.find_packages(),
+    long_description = longdescription,
+    url='https://github.com/andrefs/cetem_publico',
+    project_urls={
+        'Bug Reports': 'https://github.com/andrefs/cetem_publico/issues',
+        'Source': 'https://github.com/andrefs/cetem_publico/',
+    },
+)
